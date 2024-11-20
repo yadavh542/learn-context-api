045090566131496677fc03756ff1aa038eded09c
hemant yadav <yadavh542@gmail.com>
2024-11-20 16:16:37 +0530

    animation added

---
+++ src/features/projects/animation/Animation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Animation.css";
+
+const Animation = () => {
+  return (
+    <div className='parent'>
+        <div className='child'></div>
+    </div>
+  )
+}
+
+export default Animation
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Grid from '../features/projects/grid/Grid'
 import Flex from '../features/projects/flexbox/Flex'
+import Animation from '../features/projects/animation/Animation'
 
 const Home = () => {
   return (
     <div>
-      <Flex/>
+      <Animation/>
     </div>
   )
 }
